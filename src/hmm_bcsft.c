e431497294ea7190836aee82bfda1a91dc044d10
Frogee <ryanabashbash@tamu.edu>
2014-03-17 13:11:51 -0500

    Testing modification of single function

--- src/hmm_bcsft.c
+++ src/hmm_bcsft.c
@@ -1306,67 +1306,53 @@ void prob_bcs(double rf, int s, double *transpr)
 void prob_ft(double rf, int t, double *transpr)
 {
   int k;
-  double t1,t2,t1m2,w,w2,r2,rw;
-  double beta,gamma,beta1,sbeta1,sgamma1,SDt,SEt,sbetaBA,gamma1,beta2m1;
-  double H,H1; /* Added by RFM */
-  //warning("Within prob_ft_exHet using het value of %f", *het);
-  /* compute transition probabilities to leave double het states */
-  t1 = t - 1.0;
-  t2 = R_pow(2.0, t); /* 2^t */
-  t1m2 = 2.0 / t2;
-  w = 1.0 - rf;
-  w2 = w * w;
-  r2 = rf * rf;
-  rw = w * rf;
-  H = 0.80; /* Added by RFM */
-  H1 = ((1 - H) / 2);
+  double t1;
+  double r = rf;
+  double B_11, B_12, B_14, B_22, B_23;
+  double d;
+  double h =0.5; //*het;
+  double u;
+  double hpowt;
+  double r2, r3, r4, r5, u2, u3, u4, d2;
 
+  u=(2.0*h)/(2.0-2.0*h);
+
+  hpowt = pow(h, t);
+  r2 = pow(r, 2);
+  r3 = pow(r, 3);
+  r4 = pow(r, 4);
+  r5 = pow(r, 5);
+
+  u2 = pow(u, 2);
+  u3 = pow(u, 3);
+  u4 = pow(u, 4);
+
+  d=2.0*(pow((1.0-r),2))+8.0*u*r*(1-r)+ 2.0*(r2)+ 2.0*(u2)*((pow((1.0-r),2.0))+(r2));
+
+  d2 = pow(d, 2);
+
+  t1 = t - 1.0;
   for(k=0; k<10; k++)
     transpr[k] = 0.0;
 
-  /* A = prob go from AB.ab to AB.AB at step t */
-  /* D1 -> Dk or Ek -> Ak+1 -> At OR D1 -> Dk or Ek -> Bk+1 -> Ak+s -> At */
-  beta = (w2 + r2) / 2.0; 
-  gamma = (w2 - r2) / 2.0; 
-  beta1 = R_pow(beta, t1);
-  gamma1 = R_pow((w2 - r2) / 2.0, t1);
-  /* beta1 = prob still in D or E at step t */
-  /* sbeta1 = sum of beta1[k] from 1 to t-1 */
-  /* Dt and Et depend on sbeta1 and sgamma1 */
-  sbeta1 = (1.0 - beta1) / (1.0 - beta);
-  sgamma1 = (1.0 - R_pow(gamma, t1)) / (1.0 - gamma);
-  SDt = (sbeta1 + sgamma1) / 8.0;
-  SEt = (sbeta1 - sgamma1) / 8.0;
-  beta2m1 = 1.0 - 2.0 * beta;
+  B_11 = (1.0/2.0)*((4.0*r4*u2)/(d2 - 4.0*d*r2*u2 + 8.0*d*r*u2 - 4.0*d*u2 - 16.0*r3*u4 + 24.0*r2*u4 - 16.0*r*u4 + 4.0*u4) - (8.0*(h/2.0 - 1.0/2.0)*(- u*r2 + u*r))/(d - 4.0*r2*u2 - d*h + 2.0*h*u2 + 4.0*r*u2 - 2.0*u2 - 4.0*h*r*u2 + 4.0*h*r2*u2) + (2.0*pow((r - 1.0),2.0)*(- 2.0*r2*u2 + 4.0*r*u2 - 2.0*u2 + d))/(d2 - 4.0*d*r2*u2 + 8.0*d*r*u2 - 4.0*d*u2 - 16.0*r3*u4 + 24.0*r2*u4 - 16.0*r*u4 + 4.0*u4) + (4.0*hpowt*(- u*r2 + u*r))/(h*(4.0*r2*u2 - 4.0*r*u2 + 2.0*u2 - d*h)) - (d*pow((-(2.0*u2*(2.0*r - 1.0))/d),t))/(2.0*u2*(d + 4.0*r*u2 - 2.0*u2)) - (d*pow(((2.0*u2*(2.0*r2 - 2.0*r + 1.0))/d),t)*(16.0*r2*u2 - 16.0*r3*u2 + 8.0*r4*u2 - d*h - 8.0*r*u2 + 2.0*u2 - 4.0*d*r2*u + 2.0*d*h*r + 4.0*d*r*u - 2.0*d*h*r2 - 4.0*d*h*r*u + 4.0*d*h*r2*u))/(2.0*(- 4.0*r2*u2 + 4.0*r*u2 - 2.0*u2 + d)*(8.0*r4*u4 - 16.0*r3*u4 + 16.0*r2*u4 - 2.0*d*h*r2*u2 - 8.0*r*u4 + 2.0*d*h*r*u2 + 2.0*u4 - d*h*u2)));
 
-  /* old code--make sure new code works first */
-  /* w2pr2 = (w2 + r2); */
-  /* sbetaBA = (rw / 2.0) * (sbeta1 - 2.0 * ((2.0 / t2) - beta1) / (1.0 - 2.0 * beta)); */
-  /* transpr[1] = (2.0 * rw / t2) * ((1.0 - R_pow(w2pr2, t1)) / (1 - w2pr2)); */
+  B_12 = (-1.0/4.0)*((8.0*hpowt*(- u*r2 + u*r))/(h*(4.0*r2*u2 - 4.0*r*u2 + 2.0*u2 - d*h)) - (4.0*d*(- r2 + r)*pow(((4.0*r2*u2 - 4.0*r*u2 + 2*u2)/d),t))/((2.0*u*r2 - 2.0*u*r + u)*(4.0*r2*u2 - 4.0*r*u2 + 2.0*u2 - d*h)));
+	  
+  B_14 = (1.0/2.0)*((2.0*r2*(- 2.0*r2*u2 + 4.0*r*u2 - 2.0*u2 + d))/(d2 - 4.0*d*r2*u2 + 8.0*d*r*u2 - 4.0*d*u2 - 16.0*r3*u4 + 24.0*r2*u4 - 16.0*r*u4 + 4.0*u4) - (8.0*(h/2.0 - 1.0/2.0)*(- u*r2 + u*r))/(d - 4.0*r2*u2 - d*h + 2.0*h*u2 + 4.0*r*u2 - 2.0*u2 - 4.0*h*r*u2 + 4.0*h*r2*u2) + (4.0*hpowt*(- u*r2 + u*r))/(h*(4.0*r2*u2 - 4.0*r*u2 + 2.0*u2 - d*h)) + (d*pow((-(2.0*u2*(2.0*r - 1.0))/d),t))/(2.0*u2*(d + 4.0*r*u2 - 2.0*u2)) + (4.0*r2*u2*pow((r - 1.0),2.0))/(d2 - 4.0*d*r2*u2 + 8.0*d*r*u2 - 4.0*d*u2 - 16.0*r3*u4 + 24.0*r2*u4 - 16.0*r*u4 + 4.0*u4) - (d*pow(((2.0*u2*(2.0*r2 - 2.0*r + 1.0))/d),t)*(16.0*r2*u2 - 16.0*r3*u2 + 8.0*r4*u2 - d*h - 8.0*r*u2 + 2.0*u2 - 4.0*d*r2*u + 2.0*d*h*r + 4.0*d*r*u - 2.0*d*h*r2 - 4.0*d*h*r*u + 4.0*d*h*r2*u))/(2.0*(- 4.0*r2*u2 + 4.0*r*u2 - 2.0*u2 + d)*(8.0*r4*u4 - 16.0*r3*u4 + 16.0*r2*u4 - 2.0*d*h*r2*u2 - 8.0*r*u4 + 2.0*d*h*r*u2 + 2.0*u4 - d*h*u2)));
 
-  double s2beta1,sbeta2,s2beta2;
-  s2beta1 = (t1m2 - beta1) / beta2m1;     /* sum from 1 to t-1 of of (2*beta)^(k-1). */
-  //transpr[1] = rw * s2beta1;
-  transpr[1] = (H * H1) * (rw * s2beta1);                            /* PfB1 = PfDB */
-  transpr[6] = transpr[1];                              /* PfB0 = PfB1 */
+  B_22 =(d*pow(((2.0*u2*(2.0*r2 - 2.0*r + 1.0))/d),t))/(4.0*(2.0*r2*u2 - 2.0*r*u2 + u2)) - (d*pow((-(2.0*u2*(2.0*r - 1.0))/d),t))/(4.0*u2*(2.0*r - 1.0));
 
-  /* sbetaBA = sum beta1[k] * rw/2 * prob(B->A in remaining steps) */
-  sbeta2 = 0.0;
-  if(t > 2.0) sbeta2 = (1.0 - beta1 / beta) / (1.0 - beta); /* sum of beta^(k-1) from 1 to (t-1) */
-  s2beta2 = (2.0 * t1m2 - (beta1 / beta)) / beta2m1;        /* sum of (2*beta)^(k-1) from 1 to t-2 */
-  sbetaBA = 0.5 * rw * (sbeta2 - s2beta2);
-
-  //transpr[0] = SDt * w2 + SEt * r2 + sbetaBA;           /* PfA1 = PfDA + PfDEA + PfDBA */
-  transpr[0] = (H1 * H1) * (SDt * w2 + SEt * r2 + sbetaBA);
-  transpr[5] = transpr[0];                              /* PfA0 = PfA1 */
-
-  //transpr[2] = SDt * r2 + SEt * w2 + sbetaBA;
-  transpr[2] = (H1 * H1) * (SDt * r2 + SEt * w2 + sbetaBA);           /* PfbC = PfDC + PfDEC + PfDBC */
-  //transpr[3] = (beta1 + gamma1) / 2.0;                  /* PfD = PfDD */
-  //transpr[4] = (beta1 - gamma1) / 2.0;                  /* PfE = PfDE */
-  transpr[3] = (H * H) * (beta1 + gamma1) / 2.0; 
-  transpr[4] = (H * H) * (beta1 - gamma1) / 2.0;
-
+  B_23 =(d*pow(((2.0*u2*(2.0*r2 - 2.0*r + 1.0))/d),t))/(4.0*(2.0*r2*u2 - 2.0*r*u2 + u2)) + (d*pow((-(2.0*u2*(2.0*r - 1.0))/d),t))/(4.0*u2*(2.0*r - 1.0));
+  
+  transpr[0] = B_11;
+  transpr[1] = B_12;
+  transpr[2] = B_14;
+  transpr[3] = B_22;
+  transpr[4] = B_23;
+  transpr[5] = B_11;
+  transpr[6] = B_12;
+      
   /* marginal probabilities for one marker */
   transpr[8] = -t1 * M_LN2;                             /* Aa */
   transpr[7] = log1p(-exp(transpr[8])) - M_LN2;         /* AA */

7f2c9a0cc087bd9a85b4bf07805c0d1308fb2d3a
Frogee <ryanabashbash@tamu.edu>
2014-03-19 11:02:49 -0500

    modified H

--- src/hmm_bcsft.c
+++ src/hmm_bcsft.c
@@ -1370,9 +1370,10 @@ void prob_ft(double rf, int t, double *transpr)
   double r = rf;
   double B_11, B_12, B_14, B_22, B_23;
   double d;
-  double h = 0.719685673;  //e^(ln(percentage)/(t-1));
+  //double h = 0.719685673;  //e^(ln(percentage)/(t-1));
   //double h = 0.6059627021;
   //double h = 0.50;
+  double h = 0.6812920691;
   double u;
   double hpowt;
   double r3, r4, r5, u2, u3, u4, d2;

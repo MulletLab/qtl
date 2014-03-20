49d5513894ffffcd38abcbbad31210d8235d3f9e
Frogee <ryanabashbash@tamu.edu>
2014-03-20 18:59:33 -0500

    P

--- src/hmm_util.c
+++ src/hmm_util.c
@@ -115,12 +115,12 @@ void forward_prob(int i, int n_mar, int n_gen, int curpos, int *cross_scheme, do
   if(curpos > 0) errortol = TOL;
   for(v=0; v<n_gen; v++) { //initf here is init_bcsftb; emitf is emit_bcsftb.
     alpha[v][0] = initf(v+1, cross_scheme) + emitf(Geno[0][i], v+1, errortol, cross_scheme);
-    //Rprintf("alpha[v][0]:");
-    //sprintf(verboseString, "%f", alpha[v][0]);
-    //Rprintf(verboseString);
-    //Rprintf("\t");
+    Rprintf("alpha[v][0]:");
+    sprintf(verboseString, "%f", alpha[v][0]);
+    Rprintf(verboseString);
+    Rprintf("\t");
   }
-  //Rprintf("\n");
+  Rprintf("\n");
   if(curpos == 0) errortol = TOL;
 
   for(j=1; j<n_mar; j++) {
